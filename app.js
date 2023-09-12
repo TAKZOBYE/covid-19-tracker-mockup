46cae248d45f0c582536e78b2289c282aea7c409
TAKZOBYE <takzobye@gmail.com>
2023-09-12 15:45:57 +0700

    Finish index page

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            url: 'http://172.20.10.2:8888/covid_tracker_sillapa/api',
+
+            infected: {
+                new: 835,
+                sum: 1837
+            },
+            heal: {
+                new: 835,
+                sum: 1837
+            },
+            recovered: {
+                new: 835,
+                sum: 1837
+            },
+            dead: {
+                new: 835,
+                sum: 1837
+            },
+
+            patients: [],
+
+            typeSelected: 'all',
+            dateSelected: null,
+
         }
+    },
+    methods: {
+        async getDataCount() {
+            const res = await fetch(`${this.url}/patient/get_count.php`, {});
+            const data = await res.json();
+
+            this.infected = data.infected;
+            this.heal = data.heal;
+            this.recovered = data.recovered;
+            this.dead = data.dead;
+        },
+        async getPatients() {
+            let targetDateFormat = '';
+
+            if (this.dateSelected) {
+                targetDateFormat = new Date(this.dateSelected).toISOString().split('T')[0];
+            }
+
+            const res = await fetch(`${this.url}/patient/index.php?type=${this.typeSelected}&date=${targetDateFormat}`, {});
+            const data = await res.json();
+
+            console.log(data);
+
+            this.patients = data;
+        }
+    },
+    watch: {
+        typeSelected() {
+            this.getPatients();
+        },
+        dateSelected() {
+            this.getPatients();
+        }
+    },
+    mounted() {
+        this.getDataCount();
+        this.getPatients();
     },
 }).mount('.wrapper');
