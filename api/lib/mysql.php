145587087ee9a1d73b8560a8a8a6eb7c2a7f2f0a
TAKZOBYE <takzobye@gmail.com>
2023-09-12 17:56:30 +0700

    Update heads

--- api/lib/mysql.php
+++ api/lib/mysql.php
@@ -1,6 +1,9 @@
 <?php 
     header('Content-Type: application/json; charset=utf-8');
-    header("Access-Control-Allow-Origin: *"); 
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 
     $servername = "localhost";
     $username = "root";
