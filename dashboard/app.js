ab373479dc32193284cfc44ccd18d52dd46dc591
TAKZOBYE <takzobye@gmail.com>
2023-09-13 16:12:15 +0700

    Finish dashboard page

--- dashboard/app.js
+++ dashboard/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             popupDisplay: false,
 
-            url: 'http://127.0.0.1:8888/covid_tracker_sillapa/api',
+            url: 'http://10.10.51.144:8888/covid_tracker_sillapa/api',
 
             infected: {
                 new: 835,
@@ -31,59 +31,137 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        async getDataCount() {
-            const res = await fetch(`${this.url}/patient/get_count.php`, {});
-            const data = await res.json();
+        async checkAuth() {
+            try {
+                const res = await fetch(`${this.url}/auth/index.php`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                
+                
+                const data = await res.json();
+                
+                if (!data.isAuthenticated) {
+                    window.location.pathname = '/login';
+                }
 
-            this.infected = data.infected;
-            this.heal = data.heal;
-            this.recovered = data.recovered;
-            this.dead = data.dead;
+                console.log(data);
+
+                this.getDataCount();
+                this.getPatients();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async getDataCount() {
+            try {
+                const res = await fetch(`${this.url}/patient/get_count.php`, {});
+                const data = await res.json();
+
+                this.infected = data.infected;
+                this.heal = data.heal;
+                this.recovered = data.recovered;
+                this.dead = data.dead;
+            } catch (error) {
+                console.log(error);
+            }
         },
         async getPatients() {
-            let targetDateFormat = '';
+            try {
+                let targetDateFormat = '';
 
-            if (this.dateSelected) {
-                targetDateFormat = new Date(this.dateSelected).toISOString().split('T')[0];
+                if (this.dateSelected) {
+                    targetDateFormat = new Date(this.dateSelected).toISOString().split('T')[0];
+                }
+
+                const res = await fetch(`${this.url}/patient/index.php?type=${this.typeSelected}&date=${targetDateFormat}`, {});
+                const data = await res.json();
+
+                console.log(data);
+
+                this.patients = data;
+            } catch (error) {
+                console.log(error);
             }
-
-            const res = await fetch(`${this.url}/patient/index.php?type=${this.typeSelected}&date=${targetDateFormat}`, {});
-            const data = await res.json();
-
-            console.log(data);
-
-            this.patients = data;
         },
         editPatient(data) {
             this.patient = { ...data };
-            this.popupDisplay = true;
+            this.popupDisplay = 'edit';
         },
-        async confirmPatient() {
-            const res = await fetch(`${this.url}/patient/edit.php`, {
+        async deletePatient() {
+            const res = await fetch(`${this.url}/patient/delete.php`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    patientId: this.patient.patientId,
-                    firstName: this.patient.firstName,
-                    lastName: this.patient.lastName,
-                    age: this.patient.age,
-                    sex: this.patient.sex,
-                    hospitalId: this.patient.hospitalId,
-                    // infectedDate: this.patient.infectedDate,
-                    // healDate: this.patient.healDate,
-                    // recoveredDate: this.patient.recoveredDate,
-                    // deadDate: this.patient.deadDate,
+                    patientId: this.patient.patientId
                 })
             });
 
-            const data = await res.json();
+            await res.json();
+            await this.getPatients();
 
-            console.log(data);
+            this.patient = {};
+            this.popupDisplay = false;
+        },
+        async confirmPatient() {
+            try {
+                const url = this.popupDisplay == 'add' ? `${this.url}/patient/add.php` : `${this.url}/patient/edit.php`
 
-            // console.log(data);
-        }
+                const res = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        patientId: this.patient.patientId,
+                        firstName: this.patient.firstName,
+                        lastName: this.patient.lastName,
+                        age: this.patient.age,
+                        sex: this.patient.sex,
+                        hospitalId: this.patient.hospitalId,
+                        infectedDate: this.patient.infectedDate,
+                        healDate: this.patient.healDate,
+                        recoveredDate: this.patient.recoveredDate,
+                        deadDate: this.patient.deadDate,
+                    })
+                });
+
+                const data = await res.text();
+
+                console.log(data);
+
+                await this.getPatients();
+
+                this.patient = {};
+                this.popupDisplay = false;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async logout () {
+            try {
+                const res = await fetch(`${this.url}/auth/logout.php`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ })
+                });
+
+                await res.json();
+
+                this.patient = {};
+                this.popupDisplay = false;
+
+                this.checkAuth();
+            } catch (error) {
+                console.log(error);
+            }
+        },
     },
     watch: {
         typeSelected() {
@@ -94,7 +172,6 @@ const app = Vue.createApp({
         }
     },
     mounted() {
-        this.getDataCount();
-        this.getPatients();
+        this.checkAuth();
     },
 }).mount('.wrapper');
